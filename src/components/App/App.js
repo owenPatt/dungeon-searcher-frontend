8a230c736c1b68053ab9f28aeb80dc8e5ae52714
Owen Patterson <owen.patt.8@gmail.com>
2024-02-20 09:18:55 -0500

    Added loading for the API call

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "../Header/Header";
 import Nav from "../Nav/Nav";
 import Main from "../Main/Main";
+import React, { useState } from "react";
 
 function App() {
   return (
